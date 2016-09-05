6b43b0c2a4ce5d288bd34be5a99b1e3925816d09
Pablo <pabloezeromano@gmail.com>
2016-09-04 23:46:58 -0300

    Ejercicio de la clase 01

---
+++ Ejercicio Clase 01/js/ejercicio.js
@@ -0,0 +1,17 @@
+var name = prompt("Ingrese nombre");
+alert('Hola '+name+'!!!');
+
+var inputNumberA = parseInt(prompt("Ingrese un numero"));
+var storageNumber = 15;
+var result = inputNumberA + storageNumber;
+alert('La suma entre ' + inputNumberA + ' y ' + storageNumber + ' es ' + result);
+
+inputNumberA = parseInt(prompt("Ingrese un numero"));
+storageNumber = 22;
+result = inputNumberA - storageNumber;
+alert('La resta entre ' + inputNumberA + ' y ' + storageNumber + ' es ' + result);
+
+inputNumberA = parseInt(prompt("Ingrese un numero"));
+var inputNumberB = parseInt(prompt("Ingrese otro numero"));
+result = inputNumberA + inputNumberB;
+alert('La suma entre ' + inputNumberA + ' y ' + inputNumberB + ' es ' + result);
